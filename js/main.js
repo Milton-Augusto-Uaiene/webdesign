6d8b7e69100fdaed180a27a63829093aefec9b11
Milton Augusto Uaiene <miltonaugusto137@gmail.com>
2023-09-07 10:42:36 +0100

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,118 @@
+(function ($) {
+    "use strict";
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Header carousel
+    $(".header-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1500,
+        items: 1,
+        dots: true,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        margin: 24,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            992:{
+                items:2
+            }
+        }
+    });
+    
+})(jQuery);
+
+
+
+let userTexts = document.getElementsByClassName("uso-text");
+let userPics = document.getElementsByClassName("uso-img"); 
+
+function showReview() {
+    for(userPic of userPics) {
+        userPic.classList.remove("active-img");
+    }
+
+    for(userText of userTexts) {
+        userText.classList.remove("active-text");
+    }
+    let i = Array.from(userPics).indexOf(event.target);
+
+    userPics[i].classList.add("active-img");
+    userTexts[i].classList.add("active-text");
+}
+
+
+// PERG & RESP
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle('open');
+
+        const icon = faq.querySelector('.faq-icons i');
+        if (icon.className === 'uil uil-plus') {
+            icon.className = "uil uil-plus"
+        } else {
+            icon.className = "uil uil-plus";
+        }
+    })
+})
+
